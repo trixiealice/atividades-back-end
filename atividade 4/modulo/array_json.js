e6833aece4fe6280558d3e59234830cc7b4fd2d4
ALICE CAMPOS DA SILVA <25203690@cfp127aluno.local>
2026-03-25 13:59:40 -0300

    Atividade 4 completa

--- atividade 4/modulo/array_json.js
+++ atividade 4/modulo/array_json.js
@@ -60,20 +60,68 @@ const getEstadosRegiao = function (regiao) {
     }
   }
   
-  const capitaisBrasil = function (capital){
-    
+  const getCapitaisPais = function (){
+     const capitais = []
+
+      listaDeEstados.estados.forEach(function(estado){
+        if (estado.capital_pais){
+        capitais.push ({
+          capital_atual: estado.capital_pais.capital,
+          uf: estado.sigla,
+          descricao: estado.nome,
+          capital: estado.capital,
+          regiao: estado.regiao,
+          capital_pais_ano_inicio: estado.capital_pais.ano_inicio,
+          capital_pais_ano_termino: estado.capital_pais.ano_fim 
+
+        })
+      }  
+    })
+    return {
+      capitais:
+      capitais
+    }
+  }
+
+  function getCidades(siglaEstado){
+    let cidades = []
+    let estadoSelecionado
+
+    listaDeEstados.estados.forEach(function(estado){
+      if (String(siglaEstado).toUpperCase()==String(estado.sigla).toUpperCase()){
+      estadoSelecionado = estado
+      }
+    })
+
+    estadoSelecionado.cidades.forEach(function(cidade){
+      cidades.push(cidade.nome)
+    })
+
+    return {
+      uf: estadoSelecionado.sigla,
+      descricao: estadoSelecionado.nome,
+      quantidade_cidades: cidades.length,
+      cidades: cidades
+
+    }
   }
 
 
 
-//   console.log("Teste lista de estados:")
-//   console.log(getListaDeEstados())
+// console.log("Teste lista de estados:")
+// console.log(getListaDeEstados())
   
 // console.log("\nTeste dados do estado:")
 // console.log(getDadosEstado('SP'))
   
-//console.log("\nTeste capital:")
-//console.log(getCapitalEstado('SP'))
+// console.log("\nTeste capital:")
+// console.log(getCapitalEstado('SP'))
   
-console.log("\nTeste por região:")
-console.log(getEstadosRegiao('Sul'))
+// console.log("\nTeste por região:")
+// console.log(getEstadosRegiao('Sul'))
+
+console.log("Teste capitais pais")
+console.log(getCapitaisPais())
+
+console.log("Teste Cidades")
+console.log(getCidades("AC"))
